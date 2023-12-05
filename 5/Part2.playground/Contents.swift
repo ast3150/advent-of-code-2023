a56ecd58f0caf438e8cac3db351f478941c2319e
Alain Stulz <alain@appswithlove.com>
2023-12-05 23:44:59 +0100

    Finally, a working solution for part two

--- 5/Part2.playground/Contents.swift
+++ 5/Part2.playground/Contents.swift
@@ -1,55 +1,14 @@
 import Cocoa
 import Foundation
 
-// MARK: Models
 let startTime = Date()
-
-struct MapEntry {
-    let range: ClosedRange<Int>
-    let diff: Int
-    
-    func contains(_ int: Int) -> Bool {
-        return range.contains(int)
-    }
-}
-
-class Map {
-    var entries: [MapEntry]
-    
-    init(entries: [MapEntry]) {
-        self.entries = entries
-    }
-}
-
-extension RandomAccessCollection {
-    /// Finds such index N that predicate is true for all elements up to
-    /// but not including the index N, and is false for all elements
-    /// starting with index N.
-    /// Behavior is undefined if there is no such N.
-    func binarySearch(predicate: (Element) -> Bool) -> Index {
-        var low = startIndex
-        var high = endIndex
-        while low != high {
-            let mid = index(low, offsetBy: distance(from: low, to: high)/2)
-            if predicate(self[mid]) {
-                low = index(after: mid)
-            } else {
-                high = mid
-            }
-        }
-        return low
-    }
-}
-
 
 // MARK: Constants
 let digits: Regex = /(\d+)/
 let mapLine: Regex = /(\d+) (\d+) (\d+)/
 
-// MARK: Vars
-
-var mapList: [Map] = []
-
+typealias MapEntry = (targetStart: Int, sourceStart: Int, length: Int)
+typealias Map = [MapEntry]
 
 // MARK: Input
 
@@ -58,66 +17,103 @@ let file = try! String(contentsOf: inputFile, encoding: .utf8)
 var lines = file.components(separatedBy: .newlines)
 
 let seedsLine = lines.removeFirst()
-typealias SeedRange = (start: Int, offset: Int)
-let seedRanges: [SeedRange] = seedsLine.matches(of: /(\d+) (\d+)/).map(\.output).map { (Int($0.1)!, Int($0.2)!) }
+var seeds: [Int] = seedsLine.matches(of: digits).map(\.output).compactMap { Int($0.1) }
+
+let seedRanges: [Range<Int>] = seedsLine.matches(of: /(\d+) (\d+)/).map(\.output).map { output in
+    let start = Int(output.1)!
+    let end = start + Int(output.2)!
+    return start..<end
+}
+
+var maps: [Map] = []
+var map: Map = Map()
 
 for line in lines {
-    if line.contains(/\w+:/) {
-        mapList.append(Map(entries: []))
-    } else if let match = line.firstMatch(of: mapLine).map(\.output) {
+    if let match = line.firstMatch(of: mapLine).map(\.output) {
         let targetRangeStart = Int(match.1)!
         let sourceRangeStart = Int(match.2)!
-        let offset = Int(match.3)!
-
-        mapList.last?.entries.append(
-            MapEntry(range: sourceRangeStart...(sourceRangeStart+offset),
-                     diff: (targetRangeStart - sourceRangeStart))
-        )
+        let length = Int(match.3)!
+        map.append((targetStart: targetRangeStart, sourceStart: sourceRangeStart, length: length))
+    } else if !map.isEmpty {
+        maps.append(map)
+        map = Map()
     }
 }
 
-for map in mapList {
-    map.entries.sort(by: { $0.range.startIndex < $1.range.startIndex })
+// MARK: - Functions
+
+/// Determines the intersection of two `Range<Int>` values.
+/// - Parameters:
+///   - lhs: The first range to intersect.
+///   - rhs: The second range to intersect.
+/// - Returns: An optional `Range<Int>` representing the intersection of the two ranges, or `nil` if they do not intersect.
+func intersect(_ lhs: Range<Int>, _ rhs: Range<Int>) -> Range<Int>? {
+    let intersectionStart = max(lhs.lowerBound, rhs.lowerBound)
+    let intersectionEnd = min(lhs.upperBound, rhs.upperBound)
+    return intersectionStart < intersectionEnd ? intersectionStart..<intersectionEnd : nil
 }
 
-typealias MappedSeed = (seed: Int, location: Int)
-
-func calculateSeeds(in range: SeedRange) async -> MappedSeed {
-    var lowestSeed: MappedSeed = MappedSeed(0, Int.max)
+/// Subtracts one range from another and returns an array of ranges representing the difference.
+/// - Parameters:
+///   - lhs: The range to be subtracted from.
+///   - rhs: The range to subtract.
+/// - Returns: An array of ranges that represent the segments of `lhs` that do not overlap with `rhs`.
+func subtract(_ lhs: Range<Int>, _ rhs: Range<Int>) -> [Range<Int>] {
+    var difference: [Range<Int>] = []
     
-    for seed in range.start...(range.start + range.offset) {
-        var mappedValue = seed
+    if lhs.lowerBound < rhs.lowerBound {
+        difference.append(lhs.lowerBound..<rhs.lowerBound)
+    }
+    
+    if lhs.upperBound > rhs.upperBound {
+        difference.append(rhs.upperBound..<lhs.upperBound)
+    }
+    
+    return difference
+}
+
+func mapRanges(map: Map, unmappedRanges: [Range<Int>]) -> [Range<Int>] {
+    var mappedRanges: [Range<Int>] = []
+    var unmappedRanges = unmappedRanges
+    
+    // Iterate over all the ranges in the map
+    for entry in map {
+        var newUnmappedRanges: [Range<Int>] = []
         
-        for map in mapList {
-            let entryIndex = map.entries.binarySearch(predicate: { $0.range.lowerBound < mappedValue })
-            guard map.entries.count < entryIndex else {
-                continue
-            }
-            let entry = map.entries[entryIndex]
-            guard entry.range.contains(mappedValue) else {
-                continue
-            }
+        // Iterate over all the unmapped (source) ranges
+        for unmappedRange in unmappedRanges {
+            // The source range defined in the map entry
+            let srcRange = entry.sourceStart..<(entry.sourceStart + entry.length)
             
-            mappedValue += entry.diff
+            // We intersect the unmapped range from the source data with the map
+            if let intersection = intersect(unmappedRange, srcRange), !intersection.isEmpty {
+                // If the intersection is not empty, we can map the intersecting source data using this map entry
+                let mappedStart = intersection.lowerBound - entry.sourceStart + entry.targetStart
+                mappedRanges.append(mappedStart..<(mappedStart + intersection.count))
+                
+                // Everything not in the intersection is left for the next pass
+                newUnmappedRanges += subtract(unmappedRange, srcRange)
+            } else {
+                // Since the intersection is empty, everything is left for the next pass
+                newUnmappedRanges.append(unmappedRange)
+            }
         }
         
-        if mappedValue < lowestSeed.location {
-            print("Found seed \((seed, mappedValue))")
-            lowestSeed = (seed, mappedValue)
-        }
+        // Unmapped ranges are left for the next pass
+        unmappedRanges = newUnmappedRanges
     }
-    return lowestSeed
+    
+    // All mapped ranges are returned, all ranges that could not be mapped remain intact
+    return mappedRanges + unmappedRanges
 }
 
-Task {
-    let minSeed = await withTaskGroup(of: MappedSeed.self) { group in
-        for seedRange in seedRanges {
-            group.addTask { await calculateSeeds(in: seedRange) }
-        }
-        return await group.min(by: { $0.location < $1.location })!
-    }
-    
-    print("Lowest location: \(minSeed)")
-    
-    print("Runtime: \(Date().timeIntervalSince(startTime)) seconds")
+// MARK: - Main
+
+var currentRanges = seedRanges
+for map in maps {
+    currentRanges = mapRanges(map: map, unmappedRanges: currentRanges)
 }
+    
+print(currentRanges.min(by: { $0.lowerBound < $1.lowerBound })?.min() ?? "Not found")
+
+print("Runtime: \(Date().timeIntervalSince(startTime))")
